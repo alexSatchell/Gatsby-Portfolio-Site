34d112a260ec4962f313778fbd0742f1566e3322
alexSatchell <satchellalex@gmail.com>
2020-07-25 20:37:38 -0400

    testing keyframe issue

--- src/components/About/index.js
+++ src/components/About/index.js
@@ -14,6 +14,7 @@ import { Paragraph } from "../Typography/Paragraph"
 // import { FaCode } from "react-icons/fa"
 
 const About = () => {
+  console.log("rendered")
   return (
     <AboutContainer>
       <InfoPanel>
